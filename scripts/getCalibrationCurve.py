43bf390d7cd846a942105ff081898e12ebb1c8ff
Thomas Mueller <t.muller@cern.ch>
2018-05-11 18:34:41 +0200

    getCalibrationCurve.py: implemented looping over multiple categories

--- scripts/getCalibrationCurve.py
+++ scripts/getCalibrationCurve.py
@@ -17,9 +17,9 @@ import TauPolSoftware.CalibrationCurve.uncertainties.uncertainties as uncertaint
 
 
 def get_histogram(path):
-	splitted_path = path.split(":")
-	input_file = ROOT.TFile(splitted_path[0], "OPEN")
-	histogram = input_file.Get(splitted_path[-1])
+	input_file_path, histogram_path = path.split(":")
+	input_file = ROOT.TFile(input_file_path, "OPEN")
+	histogram = input_file.Get(histogram_path)
 	histogram.SetDirectory(0)
 	input_file.Close()
 	return histogram
@@ -68,47 +68,20 @@ def get_polarisation_values(zfitter_output_path, sin2theta_edges, energy_histogr
 		polarisation_values.append(calculate_polarisation(zfitter_output_path, sin2theta_min, sin2theta_max, energy_histogram_up, energy_histogram_down))
 	return polarisation_values
 
+def get_calibration_curve(zfitter_output_path, energy_distribution_up_path, energy_distribution_down_path, sin2theta_values, sin2theta_edges, output_path):
 
-if __name__ == "__main__":
+	zfitter_output_path = os.path.expandvars(zfitter_output_path)
+	energy_distribution_up_path = os.path.expandvars(energy_distribution_up_path)
+	energy_distribution_down_path = os.path.expandvars(energy_distribution_down_path)
+	output_path = os.path.expandvars(output_path)
 	
-	parser = argparse.ArgumentParser(description="Combine the data of up-type and down-type quarks from ZFitter and using the NLOPDF and calculate their errors.")
+	energy_distribution_up = get_histogram(energy_distribution_up_path)
+	energy_distribution_down = get_histogram(energy_distribution_down_path)
 	
-	parser.add_argument("-z", "--zfitter-output", default="$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/data/zfitter.root",
-	                    help="ZFitter output *.root file. [Default: %(default)s]")
-	
-	parser.add_argument("-u", "--energy-distribution-up", default="$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/data/energy_distribution.root:up",
-	                    help="Histogram containing u ubar -> Z events in bins of the true Z boson mass (u=u,c). Format: path/to/file.root:path/to/histogram.")
-	parser.add_argument("-d", "--energy-distribution-down", default="$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/data/energy_distribution.root:down",
-	                    help="Histogram containing d dbar -> Z events in bins of the true Z boson mass (d=d,s,b). Format: path/to/file.root:path/to/histogram.")
-	
-	parser.add_argument("--sin2theta-min", type=float, default=0.150,
-	                    help="Min. sin^2 theta_W value for scan. [Default: %(default)s]")
-	parser.add_argument("--sin2theta-max", type=float, default=0.300,
-	                    help="Max. sin^2 theta_W value for scan. [Default: %(default)s]")
-	parser.add_argument("--sin2theta-delta", type=float, default=0.0025,
-	                    help="Sin^2 theta_W step size for scan. [Default: %(default)s]")
-	
-	parser.add_argument("-o", "--output", default="$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/data/calibration_curve.root",
-	                    help="Output *.root file. [Default: %(default)s]")
-	
-	args = parser.parse_args()
-	args.zfitter_output = os.path.expandvars(args.zfitter_output)
-	args.energy_distribution_up = os.path.expandvars(args.energy_distribution_up)
-	args.energy_distribution_down = os.path.expandvars(args.energy_distribution_down)
-	args.output = os.path.expandvars(args.output)
-	
-	sin2theta_values = list(numpy.arange(args.sin2theta_min, args.sin2theta_max+args.sin2theta_delta, args.sin2theta_delta))
-	sin2theta_edges = list(numpy.arange(args.sin2theta_min-0.5*args.sin2theta_delta, args.sin2theta_max+args.sin2theta_delta, args.sin2theta_delta))
-
-	energy_distribution_up = get_histogram(args.energy_distribution_up)
-	energy_distribution_down = get_histogram(args.energy_distribution_down)
-	
-	polarisation_values = get_polarisation_values(args.zfitter_output, sin2theta_edges, energy_distribution_up, energy_distribution_down)
+	polarisation_values = get_polarisation_values(zfitter_output_path, sin2theta_edges, energy_distribution_up, energy_distribution_down)
 	
 	final_calibration = {uncertainties.ufloat(sin2theta_value, 0.0) : uncertainties.ufloat(polarisation_value, 0.0) for sin2theta_value, polarisation_value in zip(sin2theta_values, polarisation_values)}
 	sorted_keys = sorted(final_calibration.keys(), key=lambda sin2theta: sin2theta.nominal_value)
-	
-	output_file = ROOT.TFile(args.output, "RECREATE")
 	
 	n_points = len(final_calibration)
 	
@@ -143,14 +116,75 @@ if __name__ == "__main__":
 	graph_sin2theta_vs_pol.GetXaxis().SetTitle("mixing angle")
 	graph_sin2theta_vs_pol.GetYaxis().SetTitle("average polarisation -1#sigma")
 	
-	output_file.cd()
-	tools.write_object(output_file, graph_sin2theta_vs_pol, "sin2theta_vs_pol")
-	tools.write_object(output_file, graph_pol_vs_sin2theta, "pol_vs_sin2theta")
-	tools.write_object(output_file, graph_sin2theta_vs_pol_plus_sigma, "sin2theta_vs_pol_plus_sigma")
-	tools.write_object(output_file, graph_sin2theta_vs_pol_minus_sigma, "sin2theta_vs_pol_minus_sigma")
-	tools.write_object(output_file, graph_pol_plus_sigma_vs_sin2theta, "pol_plus_sigma_vs_sin2theta")
-	tools.write_object(output_file, graph_pol_minus_sigma_vs_sin2theta, "pol_minus_sigma_vs_sin2theta")
+	output_file_path, root_directory = output_path.split(":")
+	output_file = ROOT.TFile(output_file_path, "UPDATE")
 	
-	output_file.Close()
-	print "Saved outputs in\n\t{output_file}".format(output_file=args.output)
+	tools.write_object(output_file, graph_sin2theta_vs_pol, os.path.join(root_directory, "sin2theta_vs_pol"))
+	tools.write_object(output_file, graph_pol_vs_sin2theta, os.path.join(root_directory, "pol_vs_sin2theta"))
+	tools.write_object(output_file, graph_sin2theta_vs_pol_plus_sigma, os.path.join(root_directory, "sin2theta_vs_pol_plus_sigma"))
+	tools.write_object(output_file, graph_sin2theta_vs_pol_minus_sigma, os.path.join(root_directory, "sin2theta_vs_pol_minus_sigma"))
+	tools.write_object(output_file, graph_pol_plus_sigma_vs_sin2theta, os.path.join(root_directory, "pol_plus_sigma_vs_sin2theta"))
+	tools.write_object(output_file, graph_pol_minus_sigma_vs_sin2theta, os.path.join(root_directory, "pol_minus_sigma_vs_sin2theta"))
+	
+	print "Saved outputs in {output_path}".format(output_path=output_path)
+
+
+if __name__ == "__main__":
+	
+	channels = ["tt", "mt", "et"]
+	categories = [
+			"a1", "a1_1", "a1_2",
+			"rho", "rho_1", "rho_2",
+			"oneprong", "oneprong_1", "oneprong_2",
+			"combined_a1_a1", "combined_a1_rho", "combined_a1_oneprong",
+			"combined_rho_rho", "combined_rho_oneprong",
+			"combined_oneprong_oneprong"
+	]
+	default_directories = [channel+"_"+category for channel in channels for category in categories]
+	default_energy_distributions_file = "$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/data/energy_distributions.root"
+	default_energy_distributions_up = [default_energy_distributions_file+":"+os.path.join(directory, "up") for directory in default_directories]
+	default_energy_distributions_down = [default_energy_distributions_file+":"+os.path.join(directory, "down") for directory in default_directories]
+	default_outputs = [default_energy_distributions_file+":"+directory for directory in default_directories]
+	
+	parser = argparse.ArgumentParser(description="Combine the data of up-type and down-type quarks from ZFitter and using the NLOPDF and calculate their errors.")
+	
+	parser.add_argument("-z", "--zfitter-output", default="$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/data/zfitter.root",
+	                    help="ZFitter output *.root file. [Default: %(default)s]")
+	
+	parser.add_argument("-u", "--energy-distributions-up", nargs="+",
+	                    default=default_energy_distributions_up,
+	                    help="Histogram(s) containing u ubar -> Z events in bins of the true Z boson mass (u=u,c). Format: path/to/file.root:path/to/histogram. [Default: %(default)s]")
+	parser.add_argument("-d", "--energy-distributions-down", nargs="+",
+	                    default=default_energy_distributions_down,
+	                    help="Histogram(s) containing d dbar -> Z events in bins of the true Z boson mass (d=d,s,b). Format: path/to/file.root:path/to/histogram. [Default: %(default)s]")
+	
+	parser.add_argument("--sin2theta-min", type=float, default=0.150,
+	                    help="Min. sin^2 theta_W value for scan. [Default: %(default)s]")
+	parser.add_argument("--sin2theta-max", type=float, default=0.300,
+	                    help="Max. sin^2 theta_W value for scan. [Default: %(default)s]")
+	parser.add_argument("--sin2theta-delta", type=float, default=0.0025,
+	                    help="Sin^2 theta_W step size for scan. [Default: %(default)s]")
+	
+	parser.add_argument("-o", "--outputs", nargs="+",
+	                    default=default_outputs,
+	                    help="Output *.root file(s) and path(s) inside. Format: path/to/file.root:path/to/directory [Default: %(default)s]")
+	
+	args = parser.parse_args()
+	
+	sin2theta_values = list(numpy.arange(args.sin2theta_min, args.sin2theta_max+args.sin2theta_delta, args.sin2theta_delta))
+	sin2theta_edges = list(numpy.arange(args.sin2theta_min-0.5*args.sin2theta_delta, args.sin2theta_max+args.sin2theta_delta, args.sin2theta_delta))
+	
+	for energy_distribution_up, energy_distribution_down, output_path in zip(
+		args.energy_distributions_up,
+		args.energy_distributions_down,
+		args.outputs
+	):
+		get_calibration_curve(
+				args.zfitter_output,
+				energy_distribution_up,
+				energy_distribution_down,
+				sin2theta_values,
+				sin2theta_edges,
+				output_path
+		)
 
