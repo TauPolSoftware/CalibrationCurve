6aec7cf3591a926ee97dfd0769a23e16f400558b
Thomas Mueller <t.muller@cern.ch>
2018-05-12 14:07:28 +0200

    getCalibrationCurve.py, runZFitter.py: split in script+python file

---
+++ python/runzfitter.py
@@ -0,0 +1,105 @@
+
+# -*- coding: utf-8 -*-
+
+import numpy
+import os
+import string
+import tempfile
+
+import ROOT
+
+import TauPolSoftware.CalibrationCurve.tools as tools
+
+
+def get_zfitter_executable(arguments):
+	quark_type = arguments[0]
+	sin2theta_value = arguments[1]
+	sin2theta_label = arguments[2]
+	n_energy_values = arguments[3]
+	energy_min = arguments[4]
+	energy_delta = arguments[5]
+	
+	tmp_dir = tempfile.mkdtemp(prefix="zfitter_{quark_type}_{sin2theta_label}_".format(quark_type=quark_type, sin2theta_label=sin2theta_label))
+	executable = os.path.join(tmp_dir, "zfitr642.exe")
+	
+	dizet_template = None
+	with open(os.path.expandvars("$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/zFitter/FilesToSubstitute/dizet6_42.f")) as dizet_file:
+		dizet_template = string.Template(dizet_file.read())
+	
+	dizet_code = dizet_template.safe_substitute(
+			replacement_sin2theta=str(sin2theta_value)
+	)
+	
+	with open(os.path.expandvars("$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/zFitter/dizet6_42.f"), "w") as dizet_file:
+		dizet_file.write(dizet_code)
+	
+	zfusr_template = None
+	with open(os.path.expandvars("$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/zFitter/FilesToSubstitute/zfusr6_42{quark_type}.f").format(quark_type=quark_type.lower())) as zfusr_file:
+		zfusr_template = string.Template(zfusr_file.read())
+	
+	zfusr_code = zfusr_template.safe_substitute(
+			replacement_sin2theta_label=sin2theta_label,
+			replacement_n_energy_values=n_energy_values,
+			replacement_energy_min=energy_min,
+			replacement_energy_delta=energy_delta
+	)
+	
+	with open(os.path.expandvars("$CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/zFitter/zfusr6_42.f"), "w") as zfusr_file:
+		zfusr_file.write(zfusr_code)
+	
+	# compile ZFitter
+	os.system(os.path.expandvars("cd $CMSSW_BASE/src/TauPolSoftware/CalibrationCurve/zFitter && g77 -g -fno-automatic -fdollar-ok -fno-backslash -finit-local-zero -fno-second-underscore -fugly-logint -ftypeless-boz *.f -o {executable}".format(executable=executable)))
+	
+	return [executable, quark_type, sin2theta_value, sin2theta_label]
+
+
+def run_zfitter(arguments):
+	executable = arguments[0]
+	quark_type = arguments[1]
+	sin2theta_value = arguments[2]
+	sin2theta_label = arguments[3]
+	
+	print "sin^2 theta_W={value} run...".format(value=sin2theta_value)
+	
+	# run ZFitter
+	tmp_dir = os.path.dirname(executable)
+	tmp_executable = os.path.basename(executable)
+	os.system("cd {tmp_dir} && ./{executable}".format(tmp_dir=tmp_dir, executable=tmp_executable))
+	
+	# add sin^2 theta_W column
+	zfitter_dat_filename = os.path.join(tmp_dir, "PolarizationAndXsecQuark{quark_type}_{value}.dat".format(quark_type=quark_type, value=sin2theta_label))
+	zfitter_output = numpy.genfromtxt(fname=zfitter_dat_filename)
+	zfitter_output = numpy.concatenate((zfitter_output, numpy.full((zfitter_output.shape[0], 1), sin2theta_value)), axis=1)
+	numpy.savetxt(zfitter_dat_filename, zfitter_output)
+	
+	return {quark_type : zfitter_dat_filename}
+
+
+def collect_zfitter_outputs(zfitter_outputs, args):
+	
+	output_file = ROOT.TFile(args.output, "RECREATE")
+	
+	for quark_type, tmp_zfitter_outputs in zfitter_outputs.iteritems():
+		# fill tree
+		output_tree = ROOT.TTree()
+		output_tree.SetName("zfitter_{quark_type}".format(quark_type=quark_type.replace("D", "down").replace("U", "up")))
+		for zfitter_output in tmp_zfitter_outputs:
+			output_tree.ReadFile(zfitter_output, "energy:xsec:pol:sin2theta")
+			os.system("rm -r {zfitter_output}".format(zfitter_output=os.path.dirname(zfitter_output)))
+		tools.write_object(output_file, output_tree, output_tree.GetName())
+		
+		# fill histogram
+		output_tree.Draw("pol:sin2theta:energy>>{tree_name}_pol_vs_sin2theta_vs_energy({energy_bins},{energy_low},{energy_high},{sin2theta_bins},{sin2theta_low},{sin2theta_high})".format(
+				tree_name = output_tree.GetName(),
+				energy_bins = len(energy_values),
+				energy_low = (args.energy_min - (args.energy_delta/2.0)),
+				energy_high = (args.energy_max + (args.energy_delta/2.0)),
+				sin2theta_bins = len(sin2theta_values),
+				sin2theta_low = (args.sin2theta_min - (args.sin2theta_delta/2.0)),
+				sin2theta_high = (args.sin2theta_max + (args.sin2theta_delta/2.0))
+		), "", "prof goff")
+	
+	output_file.Write()
+	output_file.Close()
+	print "Saved outputs in\n\t{output_file}".format(output_file=args.output)
+
--- scripts/getCalibrationCurve.py
+++ scripts/getCalibrationCurve.py
@@ -2,131 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import hashlib
 import numpy
-import os
 
-import ROOT
-
-import TauPolSoftware.CalibrationCurve.tools as tools
-
-# the calculations here use the uncertainties package for the error propagation
-# use <return value>.nominal_value to retrive the central value
-# http://uncertainties-python-package.readthedocs.io/en/latest/user_guide.html
-import TauPolSoftware.CalibrationCurve.uncertainties.uncertainties as uncertainties
-
-
-def get_histogram(path):
-	input_file_path, histogram_path = path.split(":")
-	input_file = ROOT.TFile(input_file_path, "OPEN")
-	histogram = input_file.Get(histogram_path)
-	histogram.SetDirectory(0)
-	input_file.Close()
-	return histogram
-
-def get_zfitter_polarisation_histogram(zfitter_output_path, sin2theta_min, sin2theta_max, energy_binning_histogram, quark_type):
-	zfitter_tree = ROOT.TChain("zfitter_{quark_type}".format(quark_type=quark_type))
-	zfitter_tree.Add(zfitter_output_path)
-	
-	histogram_name = "zfitter_polarisation_{quark_type}".format(quark_type=quark_type)+hashlib.md5("".join([str(item) for item in [zfitter_output_path, sin2theta_min, sin2theta_max, energy_binning_histogram, quark_type]])).hexdigest()
-	binning = tools.get_binning(energy_binning_histogram)
-	histogram = ROOT.TProfile(histogram_name, histogram_name, len(binning)-1, binning)
-	
-	zfitter_tree.Project(histogram_name, "pol:energy", "(sin2theta>={sin2theta_min})*(sin2theta<{sin2theta_max})".format(
-			sin2theta_min=sin2theta_min,
-			sin2theta_max=sin2theta_max
-	), "prof goff")
-	
-	histogram = ROOT.gDirectory.Get(histogram_name).ProjectionX() # projection to convert into TH1
-	return histogram
-
-def get_polarisation(polarisation_histogram_up, polarisation_histogram_down, energy_histogram_up, energy_histogram_down):
-	polarisation_histogram_up.Multiply(energy_histogram_up)
-	polarisation_histogram_down.Multiply(energy_histogram_down)
-	
-	integral_polarisation_energy_up = polarisation_histogram_up.Integral()
-	integral_polarisation_energy_down = polarisation_histogram_down.Integral()
-	
-	integral_energy_up = energy_histogram_up.Integral()
-	integral_energy_down = energy_histogram_down.Integral()
-	
-	numerator = (integral_polarisation_energy_up + integral_polarisation_energy_down)
-	denominator = (integral_energy_up + integral_energy_down)
-	polarisation = 0.0
-	if denominator != 0.0:
-		polarisation = numerator / denominator
-	return polarisation
-
-def calculate_polarisation(zfitter_output_path, sin2theta_min, sin2theta_max, energy_histogram_up, energy_histogram_down):
-	polarisation_histogram_up = get_zfitter_polarisation_histogram(zfitter_output_path, sin2theta_min, sin2theta_max, energy_histogram_up, "up")
-	polarisation_histogram_down = get_zfitter_polarisation_histogram(zfitter_output_path, sin2theta_min, sin2theta_max, energy_histogram_down, "down")
-	return get_polarisation(polarisation_histogram_up, polarisation_histogram_down, energy_histogram_up, energy_histogram_down)
-
-def get_polarisation_values(zfitter_output_path, sin2theta_edges, energy_histogram_up, energy_histogram_down):
-	polarisation_values = []
-	for sin2theta_min, sin2theta_max in zip(sin2theta_edges[:-1], sin2theta_edges[1:]):
-		polarisation_values.append(calculate_polarisation(zfitter_output_path, sin2theta_min, sin2theta_max, energy_histogram_up, energy_histogram_down))
-	return polarisation_values
-
-def get_calibration_curve(zfitter_output_path, energy_distribution_up_path, energy_distribution_down_path, sin2theta_values, sin2theta_edges, output_path):
-
-	zfitter_output_path = os.path.expandvars(zfitter_output_path)
-	energy_distribution_up_path = os.path.expandvars(energy_distribution_up_path)
-	energy_distribution_down_path = os.path.expandvars(energy_distribution_down_path)
-	output_path = os.path.expandvars(output_path)
-	
-	energy_distribution_up = get_histogram(energy_distribution_up_path)
-	energy_distribution_down = get_histogram(energy_distribution_down_path)
-	
-	polarisation_values = get_polarisation_values(zfitter_output_path, sin2theta_edges, energy_distribution_up, energy_distribution_down)
-	
-	final_calibration = {uncertainties.ufloat(sin2theta_value, 0.0) : uncertainties.ufloat(polarisation_value, 0.0) for sin2theta_value, polarisation_value in zip(sin2theta_values, polarisation_values)}
-	sorted_keys = sorted(final_calibration.keys(), key=lambda sin2theta: sin2theta.nominal_value)
-	
-	n_points = len(final_calibration)
-	
-	graph_values_sin2theta = numpy.array([key.nominal_value for key in sorted_keys])
-	graph_errors_sin2theta = numpy.array([key.std_dev for key in sorted_keys])
-	graph_values_pol = numpy.array([final_calibration[key].nominal_value for key in sorted_keys])
-	graph_errors_pol = numpy.array([final_calibration[key].std_dev for key in sorted_keys])
-	graph_values_pol_plus_sigma = graph_values_pol + graph_errors_pol
-	graph_values_pol_minus_sigma = graph_values_pol - graph_errors_pol
-	
-	graph_sin2theta_vs_pol = ROOT.TGraphErrors(n_points, graph_values_pol, graph_values_sin2theta, graph_errors_pol, graph_errors_sin2theta)
-	graph_sin2theta_vs_pol.GetXaxis().SetTitle("mixing angle")
-	graph_sin2theta_vs_pol.GetYaxis().SetTitle("average polarisation")
-	
-	graph_pol_vs_sin2theta = ROOT.TGraphErrors(n_points, graph_values_sin2theta, graph_values_pol, graph_errors_sin2theta, graph_errors_pol)
-	graph_sin2theta_vs_pol.GetXaxis().SetTitle("average polarisation")
-	graph_sin2theta_vs_pol.GetYaxis().SetTitle("mixing angle")
-	
-	graph_sin2theta_vs_pol_plus_sigma = ROOT.TGraph(n_points, graph_values_pol_plus_sigma, graph_values_sin2theta)
-	graph_sin2theta_vs_pol.GetXaxis().SetTitle("average polarisation +1#sigma")
-	graph_sin2theta_vs_pol.GetYaxis().SetTitle("mixing angle")
-	
-	graph_sin2theta_vs_pol_minus_sigma = ROOT.TGraph(n_points, graph_values_pol_minus_sigma, graph_values_sin2theta)
-	graph_sin2theta_vs_pol.GetXaxis().SetTitle("average polarisation -1#sigma")
-	graph_sin2theta_vs_pol.GetYaxis().SetTitle("mixing angle")
-	
-	graph_pol_plus_sigma_vs_sin2theta = ROOT.TGraph(n_points, graph_values_sin2theta, graph_values_pol_plus_sigma)
-	graph_sin2theta_vs_pol.GetXaxis().SetTitle("mixing angle")
-	graph_sin2theta_vs_pol.GetYaxis().SetTitle("average polarisation +1#sigma")
-	
-	graph_pol_minus_sigma_vs_sin2theta = ROOT.TGraph(n_points, graph_values_sin2theta, graph_values_pol_minus_sigma)
-	graph_sin2theta_vs_pol.GetXaxis().SetTitle("mixing angle")
-	graph_sin2theta_vs_pol.GetYaxis().SetTitle("average polarisation -1#sigma")
-	
-	output_file_path, root_directory = output_path.split(":")
-	output_file = ROOT.TFile(output_file_path, "UPDATE")
-	
-	tools.write_object(output_file, graph_sin2theta_vs_pol, os.path.join(root_directory, "sin2theta_vs_pol"))
-	tools.write_object(output_file, graph_pol_vs_sin2theta, os.path.join(root_directory, "pol_vs_sin2theta"))
-	tools.write_object(output_file, graph_sin2theta_vs_pol_plus_sigma, os.path.join(root_directory, "sin2theta_vs_pol_plus_sigma"))
-	tools.write_object(output_file, graph_sin2theta_vs_pol_minus_sigma, os.path.join(root_directory, "sin2theta_vs_pol_minus_sigma"))
-	tools.write_object(output_file, graph_pol_plus_sigma_vs_sin2theta, os.path.join(root_directory, "pol_plus_sigma_vs_sin2theta"))
-	tools.write_object(output_file, graph_pol_minus_sigma_vs_sin2theta, os.path.join(root_directory, "pol_minus_sigma_vs_sin2theta"))
-	
-	print "Saved outputs in {output_path}".format(output_path=output_path)
+import TauPolSoftware.CalibrationCurve.getcalibrationcurve as getcalibrationcurve
 
 
 if __name__ == "__main__":
@@ -179,7 +57,7 @@ if __name__ == "__main__":
 		args.energy_distributions_down,
 		args.outputs
 	):
-		get_calibration_curve(
+		getcalibrationcurve.get_calibration_curve(
 				args.zfitter_output,
 				energy_distribution_up,
 				energy_distribution_down,
